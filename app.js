1140b804e402a6870705549f71e015d37cc49a4a
hamza-sf <hamzamenahil@gmail.com>
2026-05-03 12:32:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,262 @@
+var btn_1 = document.querySelector("#btn-1");
+var btn_2 = document.querySelector("#btn-2");
+var btn_3 = document.querySelector("#btn-3");
+var btn_4 = document.querySelector("#btn-4");
+var btn_5 = document.querySelector("#btn-5");
+var btn_6 = document.querySelector("#btn-6");
+var btn_7 = document.querySelector("#btn-7");
+var btn_8 = document.querySelector("#btn-8");
+var btn_9 = document.querySelector("#btn-9");
+var btn_10 = document.querySelector("#btn-10");
+var btn_11 = document.querySelector("#btn-11");
+var btn_12 = document.querySelector("#btn-12");
+var btn_13 = document.querySelector("#btn-13");
+
+
+var music_1 = new Audio();
+music_1.src = "music/music-1.mp3"
+
+
+var music_2 = new Audio();
+music_2.src = "music/music-2.mp3"
+
+
+var music_3 = new Audio();
+music_3.src = "music/music-3.mp3"
+
+
+var music_4 = new Audio();
+music_4.src = "music/music-4.mp3"
+
+
+var music_5 = new Audio();
+music_5.src = "music/music-5.mp3"
+
+
+var music_6 = new Audio();
+music_6.src = "music/music-6.mp3"
+
+
+var music_7 = new Audio();
+music_7.src = "music/music-7.mp3"
+
+
+var music_8 = new Audio();
+music_8.src = "music/music-8.mp3"
+
+
+var music_9 = new Audio();
+music_9.src = "music/music-9.mp3"
+
+
+var music_10 = new Audio();
+music_10.src = "music/music-10.mp3"
+
+
+var music_11 = new Audio();
+music_11.src = "music/music-11.mp3"
+
+
+var music_12 = new Audio();
+music_12.src = "/music/Good_Morning_to_All(chosic.com).mp3"
+
+
+function sound1(){
+    music_1.play()
+    music_2.pause()
+    music_3.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+
+}
+
+function sound2(){
+    music_2.play()
+    music_1.pause()
+    music_3.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+function sound3(){
+    music_3.play()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+function sound4(){
+    music_4.play()
+    music_3.pause()
+    music_2.pause()
+    music_1.pause()
+    music_5.pause()
+    music_6.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+
+function sound5(){
+    music_5.play()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_3.pause()
+    music_6.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+
+function sound6(){
+    music_6.play()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_3.pause()
+    music_7.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+
+function sound7(){
+    music_7.play()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+}
+
+function sound8(){
+    music_1.pause()
+    music_2.pause()
+    music_3.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_7.pause()
+    music_8.play()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+}
+
+function sound9(){
+    music_9.play()
+    music_8.pause()
+    music_7.pause()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+    music_12.pause()
+    music_11.pause()
+    music_10.pause()
+}
+
+function sound10(){
+    music_10.play()
+    music_9.pause()
+    music_8.pause()
+    music_7.pause()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+    music_12.pause()
+    music_11.pause()
+
+}
+
+function sound11(){
+    music_11.play()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+    music_7.pause()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+    music_12.pause()
+}
+
+function sound12(){
+    music_12.play()
+    music_11.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+    music_7.pause()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+}
+function sound13(){
+    music_13.play()
+    music_11.pause()
+    music_12.pause()
+    music_10.pause()
+    music_9.pause()
+    music_8.pause()
+    music_7.pause()
+    music_2.pause()
+    music_1.pause()
+    music_4.pause()
+    music_5.pause()
+    music_6.pause()
+    music_3.pause()
+}
